6e74715eca8f9dda9ca21a49d70de346ca184b8b
Scr4pe <83241458+Scr4pe@users.noreply.github.com>
2021-05-06 00:34:24 +0200

    Create path function

--- Code.py
+++ Code.py
@@ -1,41 +1,67 @@
-import tkinter as tk
-from tkinter import ttk
+# Import the following Modules
+from tkinter import *
 import csv
+import os
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
+root = Tk()
+path = Entry(root,width=50)
+path.pack()
+path.insert(0,"Check Path lll")
+# No clue
+def __init__(self, master=None):
+    super().__init__(master)
+    self.master = master
+    self.pack()
+    self.create_widgets()
+# Creates following widgets
+def create_widgets(self):
+    self.hi_there = Button(self)
+    self.hi_there["text"] = "Hello World\n(click me)"
+    self.hi_there["command"] = self.say_hi
+    self.hi_there.pack(side="top")
+    self.quit = Button(self, text="QUIT", fg="red", command=self.master.destroy)
+    self.quit.pack(side="bottom")
+# Delete this
+def say_hi(self):
+    print("hi there, everyone!")
+# This function checks if your csv file exists 
+def check_path_csv():
+    # If input is ...
+    try:
+        trying = open(path.get())
+        print("file exists")
+    except IOError:
+        print("path not accessible")
+        
+#    if (path.get() == 'lol'):
+#        print("lol")
+#    # If input isnt...
+#    else:
+#        print("WTF")
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+# Start Class Application
 
-        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
-        self.quit.pack(side="bottom")
+Path_button = Button(root, text="Check CSV:", command=check_path_csv)
+Path_button.pack()
+#Path.pack(side="top",archor="nw")
 
-    def say_hi(self):
-        print("hi there, everyone!")
+#T = Text(root,height=1, width=30)
+#T.pack(side="top", anchor="nw")
+#T.insert(END,"Path:")
+#label = Label(root, text="Enter name")
 
-root = tk.Tk()
-label = ttk.Label(root, text="Enter name")
-
-text_widget = tk.Text(root, height=5, width=40)
+text_widget = Text(root, height=5, width=40,)
  
 # Create a scrollbar
-scroll_bar = tk.Scrollbar(root)
+scroll_bar = Scrollbar(root)
  
 # Pack the scroll bar
 # Place it to the right side, using tk.RIGHT
-scroll_bar.pack(side=tk.RIGHT)
+scroll_bar.pack(side="right")
  
 # Pack it into our tkinter application
 # Place the text widget to the left side
-text_widget.pack(side=tk.LEFT)
+text_widget.pack(side="left")
  
 long_text = """This is a multiline string.
 We can write this in multiple lines too!
@@ -43,10 +69,10 @@ Hello from AskPython. This is the third line.
 This is the fourth line. Although the length of the text is longer than
 the width, we can use tkinter's scrollbar to solve this problem!
 """
- 
-# Insert text into the text widget
-text_widget.insert(tk.END, long_text)
 
+# Insert text into the text widget
+text_widget.insert(END, long_text)
+root.iconbitmap(r"../CSV_Lens/lens.ico")
 # Settings
 # Title shown in the window
 root.title("CSV Lens")
@@ -55,9 +81,13 @@ root.minsize(400,400)
 # Startup Size of the window
 root.geometry("500x500")
 # Icon in the window
-root.iconbitmap("Lens.ico")
-# Background color
+#os.chdir("/CSV_Lens/code")
+#os.getcwd()
+#root.iconbitmap('/Lens.ico')
 root.configure(background = "#d9d9d9")
-#
-app = Application(master=root)
-app.mainloop()
+
+
+#root.tk.call('wm', 'iconphoto', root._w, tk.PhotoImage(file='/CSV_Lens/Ico/Lens.ico')
+# Background color
+
+root.mainloop()
