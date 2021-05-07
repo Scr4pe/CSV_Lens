24abe3c9f80bbb5de8159ca7313351baa255cb85
Scr4pe <83241458+Scr4pe@users.noreply.github.com>
2021-05-07 23:56:39 +0200

    Skip button finished csv open error

--- Code.py
+++ Code.py
@@ -12,26 +12,31 @@ Path_wrong_path_button_color = "#ae0000"
 Path_wrong_path_button_color_clicked = "#8B0000"
 Path_wrong_file_button_color = "#ae0000"
 Path_wrong_file_button_clicked = "#8B0000"
-Path_isnt_csv_message = "File is wrong\nTry a file like\n.csv"
-Path_is_wrong_message = "Path is wrong\nTry format like\n"+ "F:/User/Code/test.csv"
-
+Path_isnt_csv_message = "File is wrong\nTry a file like\nending with .csv"
+Path_is_wrong_message = "Path is wrong\nTry format like\n" + "F:/User/Code/test.csv"
+skip_button_right_color = "#659658"
+skip_button_right_color_clicked = "#006400"
+skip_button_wrong_color = "#ae0000"
+skip_button_wrong_color_clicked = "#8B0000"
+skip_is_string_message = "Input is wrong (string)\nTry input like\nInteger 1 or 2.."
+skip_is_unkown_message = "Input is wrong (unkown)\nTry input like\nInteger 1 or 2.."
 # No clue
-def __init__(self, master=None):
-    super().__init__(master)
-    self.master = master
-    self.pack()
-    self.create_widgets()
-# Creates following widgets
-def create_widgets(self):
-    self.hi_there = Button(self)
-    self.hi_there["text"] = "Hello World\n(click me)"
-    self.hi_there["command"] = self.say_hi
-    self.hi_there.pack(side="top")
-    self.quit = Button(self, text="QUIT", fg="red", command=self.master.destroy)
-    self.quit.pack(side="bottom")
-# Delete this
-def say_hi(self):
-    print("hi there, everyone!")
+#def __init__(self, master=None):
+#    super().__init__(master)
+#    self.master = master
+#    self.pack()
+#    self.create_widgets()
+## Creates following widgets
+#def create_widgets(self):
+#    self.hi_there = Button(self)
+#    self.hi_there["text"] = "Hello World\n(click me)"
+#    self.hi_there["command"] = self.say_hi
+#    self.hi_there.pack(side="top")
+#    self.quit = Button(self, text="QUIT", fg="red", command=self.master.destroy)
+#    self.quit.pack(side="bottom")
+## Delete this
+#def say_hi(self):
+#    print("hi there, everyone!")
 
 # This function checks if your path to csv is valid
 def check_path_csv():
@@ -54,18 +59,54 @@ def check_path_csv():
         Path_button.configure(background = Path_wrong_path_button_color, activebackground = Path_wrong_path_button_color_clicked)
         messagebox.showinfo(title="ERROR",message=Path_is_wrong_message)
 
+# Ask the user if this is the right start line
+def start_lines():
+    trying = open(path.get())
+    skipping = skip.get()
+    # Try value type is integer
+    try:
+        int(skipping)
+        # Change color to green
+        skip_button.configure(background = skip_button_right_color, activebackground = skip_button_right_color_clicked)
+        # 
+
+        # Open CSV file and ask if this is the first line of data not the header
+        csv_file = open(path.get())
+        csv_reader = csv.reader(csv_file)
+        while (skipping > 0):
+            csv_reader = next(csv_reader)
+            skipping = skipping - 1
+        else:
+            # if row in csv_reader:
+            messagebox.showinfo(title="Is this data ?",message = csv_reader)    
+
+
+    # If value type isnt integer or string
+    except ValueError:
+        # Try value type is string
+        try:
+            str(skipping)
+            # Change color to red
+            skip_button.configure(background = skip_button_wrong_color, activebackground = skip_button_wrong_color_clicked)
+            # Show message imput is wrong
+            messagebox.showinfo(title="ERROR",message=skip_is_string_message)
+        # If value isnt string
+        except ValueError:
+            # Change color of buttom to red
+            skip_button.configure(background = skip_button_wrong_color, activebackground = skip_button_wrong_color_clicked)
+            # Show message imput is wrong  
+            messagebox.showinfo(title="ERROR",message=skip_is_unkown_message)
+
+    # Message the user if this is right
+    # Add if statement in the messagebox
+
 # It is the basic informations every Algorithm need from the csv
 def Algo_basic():
     # import Path
     print(path_type)
-    # 
-# Ask the user if this is the right start line
-def start_lines():
-    trying = open(path.get())
-    # Message the user if this is right
-    # Add if statement in the messagebox
-    messagebox.showinfo(title="ERROR",message=trying)
-    
+    #
+
+
 
 
 root = Tk()
